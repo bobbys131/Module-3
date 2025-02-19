8a477f1efb66152facbb0dc9b607575c6e9ed581
bobbys131 <bobbyspatz1@gmail.com>
2025-02-19 16:34:52 -0600

    added GameOver Script
    added gameover script to send player back to spawn when an enemy gets them.

--- Assets/GameOver.cs
+++ Assets/GameOver.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject SpawnPoint;
+    public GameObject EnemyKillBox;
+    public string sceneName;
+    private void OnTriggerEnter(Collider collider)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (collider.gameObject == EnemyKillBox.gameObject)
+        {
+            transform.position = SpawnPoint.transform.position;
+        }
     }
 }
