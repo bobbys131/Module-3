2ee48177749a04e1af709afeb711113f8087fa60
bobbys131 <bobbyspatz1@gmail.com>
2025-02-19 17:42:38 -0600

    created winscript
    created a win script and a win zone gameobject

---
+++ Assets/WinScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinScript : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Enemy1;
+    public GameObject Enemy2;
+    public GameObject Enemy3;
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject == Player.gameObject)
+        {
+            Enemy1.SetActive(false);
+            Enemy2.SetActive(false);
+            Enemy3.SetActive(false);
+        }
+    }
+}
